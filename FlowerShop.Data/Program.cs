eb29737e58cc9c25748708deba6ac70800001204
Thebro40 <angel_paunov6@abv.bg>
2022-03-12 11:58:10 +0200

    i am have a hug dkcodik

---
+++ FlowerShop.Data/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FlowerShop.Data
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ FlowerShop.Services/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FlowerShop.Services
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
