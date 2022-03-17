4205079dd637e0af697636788e0dece011b7dfe7
Yordan K <iordan.kar2@gmail.com>
2022-03-18 00:13:37 +0200

    SaleService in stage of completion

--- FlowerShop.Services/SaleService.cs
+++ FlowerShop.Services/SaleService.cs
@@ -8,7 +8,70 @@ using System.Threading.Tasks;
 
 namespace FlowerShop.Services
 {
-    internal class SaleService
+    public class SaleService : ISaleService
     {
+        private readonly AppDbContext context;
+        private readonly FlowerService flowerService;
+        private readonly UserService userService;
+        //check if I need this constructor
+        public SaleService(AppDbContext context, FlowerService flowerService, UserService userService)
+        {
+            this.context = context;
+            this.flowerService = flowerService;
+            this.userService = userService;
+        }
+
+        public Flower GetFlowerByName(string name)
+        {
+            return this.context.Flowers.FirstOrDefault(x => x.Name == name);
+        }
+        public User GetUserByUsername(string username)
+        {
+            User user = this.context.Users.Where(x => x.Username == username).FirstOrDefault();
+            return user;
+          
+        }
+        public void BuyFlower(string flowerName, int quantity, string username)
+        {
+            Flower flower = flowerService.GetFlowerByName(flowerName);
+            User user = userService.GetUserByUsername(username);
+            if (flowerName == null)
+            {
+                throw new ArgumentException("Flower not found!");
+            }
+            if (username == null)
+            {
+                throw new ArgumentException("User not found!");
+            }
+            if (user.Balance < flower.Price * quantity)
+            {
+                throw new ArgumentException("Insufficient balance!");
+            }
+            if (flower.Quantity < quantity)
+            {
+                throw new ArgumentException("Not enought flowers!");
+            }
+            FlowerSale flowerSale = new FlowerSale();
+            flowerSale.Flower = flower;
+            flowerSale.User = user;
+            flowerSale.Quantity = quantity;
+            flowerSale.Date = DateTime.UtcNow;
+            flowerSale.Price = flower.Price;
+
+            user.Balance -= flower.Price * quantity;
+            flower.Quantity -= quantity;
+            context.Update(flower);
+            context.Update(user);
+            context.Add(flowerSale);
+            context.SaveChanges();
+        }
+
+        public List<FlowerSale> GetPurchasedFlowersByUser(string username)
+        {
+            User user = userService.GetUserByUsername(username);
+            List<FlowerSale> flowerSale = context.FlowerSales.Where(x => x.Id == user.Id).ToList();
+
+            return flowerSale;
+        }
     }
 }
